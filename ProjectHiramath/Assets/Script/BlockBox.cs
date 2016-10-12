7b6ed524faf3e691be9feacf0104708fba91a309
PMC-Yoon <yoon@pmc.co.jp>
2016-10-12 15:28:09 +0900

    バグ修正

--- ProjectHiramath/Assets/Script/BlockBox.cs
+++ ProjectHiramath/Assets/Script/BlockBox.cs
@@ -51,7 +51,8 @@ public class BlockBox : MonoBehaviour {
         float blockwidth;
         if (NumberBlock != null)
         {
-            blockwidth = NumberBlock.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f;
+            //blockwidth = NumberBlock.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f;
+            blockwidth = width / 6.0f * 0.5f;
         }
         else
         {
