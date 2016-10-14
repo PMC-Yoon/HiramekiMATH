00f455204db0dede6ae3268aef82d0d952e8e95a
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-14 17:20:55 +0900

    結合前に一度UP

--- ProjectHiramath/Assets/Script/PlayerController.cs
+++ ProjectHiramath/Assets/Script/PlayerController.cs
@@ -6,16 +6,26 @@ public class PlayerController : MonoBehaviour
 
     public float Speed;
     GameObject arrayBlcokBox;
+
+    public GameObject BlockPrefab;
+
     int PlayerX;
     private BlockBox.BLOCKBOX BlockData;
     bool bPlayerCatch;
+    bool animStart;
+    float StartTime;
+
+
+
+    Vector3 bpos;
+
 
     void Start()
     {
         PlayerX = 3;
         bPlayerCatch = false;
-
-
+        animStart = false;
+        StartTime = 0.0f;
         //   transform.position = new Vector3( transform.position.z);
 
     }
@@ -34,9 +44,36 @@ public class PlayerController : MonoBehaviour
             MoveRight();
         }
 
+
+
+
+        //データが入っている
+        if (animStart)
+        {
+
+            if (bPlayerCatch)
+            {
+
+                //                BlockData.pos;
+       
+
+            }
+
+            //リリース（上へ移動？）
+            if (!bPlayerCatch)
+            {
+
+            }
+
+
+        }
+
+
+
+
+        //位置情報をあげる
         transform.position = new Vector3(hoge.GetData(PlayerX, 8).pos.x, hoge.GetData(PlayerX, 8).pos.y + (this.GetComponent<SpriteRenderer>().bounds.size.y * 0.2f), 0);
-
-
+      
     }
 
 
@@ -63,7 +100,7 @@ public class PlayerController : MonoBehaviour
         if (!BlockData.use)
         {
         
-
+            
             BlockData = arrayBlcokBox.GetComponent<BlockBox>().GetData(PlayerX, arrayBlcokBox.GetComponent<BlockBox>().GetArrayY(PlayerX));
             if (BlockData.use)
             {
@@ -96,10 +133,37 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
+
+            bpos = arrayBlcokBox.GetComponent<BlockBox>().GetData(PlayerX, arrayBlcokBox.GetComponent<BlockBox>().GetArrayY(PlayerX)).pos;
+            bool numflg  = arrayBlcokBox.GetComponent<BlockBox>().GetData(PlayerX, arrayBlcokBox.GetComponent<BlockBox>().GetArrayY(PlayerX)).Number;
+            int datanum =  arrayBlcokBox.GetComponent<BlockBox>().GetData(PlayerX, arrayBlcokBox.GetComponent<BlockBox>().GetArrayY(PlayerX)).data;
+
+            BlockData.Block = Instantiate(BlockPrefab);
+
+            //  BlockData.Block.transform.SetParent(this.transform);
+           
+            BlockData.Block.GetComponent<BlockSelector>().NumberChanger(numflg, datanum);
+            BlockData.Block.transform.position = bpos;
+ //           StartTime = Time.timeSinceLevelLoad;
+          //  BlockData.Block.transform.Translate(0, 1, 0);
+
+            // arrayBlcokBox.GetComponent<BlockBox>().GetData(PlayerX, arrayBlcokBox.GetComponent<BlockBox>().GetArrayY(PlayerX)).Number;
+            //addData(n, m, true, nNum);
+            //キャッチ（下へ移動？）
+
+
+
+
+
+
+
             NumberCatch();
             bPlayerCatch = true;
 
+           
+
         }
+        animStart = true;
     }
   
 
