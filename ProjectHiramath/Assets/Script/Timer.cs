92653bc7047354ba94ea7f53cd47946e07f8bd86
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-14 11:58:07 +0900

    gamesystem,Timer,CatchAndRelease

---
+++ ProjectHiramath/Assets/Script/Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    public float Seconds;
+    Text text;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+
+      
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Seconds -= Time.deltaTime;
+
+        if( Seconds < 0 )
+        {
+            Seconds = 0.0f;
+        }
+
+        text.text = Seconds.ToString("000");
+        
+	
+	}
+}
