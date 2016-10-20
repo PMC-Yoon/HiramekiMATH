8c79cb3758f86bbecc3cdbc819388fbde439baa2
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-20 15:20:03 +0900

    Canvasに背景置かれてたのをスプライトに変更（ポーズ実装用）

---
+++ ProjectHiramath/Assets/Script/Menu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour {
+    public GameObject PauseMenuPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    //ポーズを有効にする関数
+    void PauseSet()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    //ポーズを無効化する関数
+    void PauseRelease()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    //ポーズ中なら解除、ポーズじゃない場合ポーズをセット
+    public void PauseAndRelease()
+    {
+        if( (int)Time.timeScale == 0)
+        {
+            PauseRelease();
+            return;
+        }
+        if( (int)Time.timeScale == 1)
+        {
+            PauseSet();
+            PauseMenuPrefab.SetActive(true);
+            return;
+        }
+    }
+
+
+
+}
