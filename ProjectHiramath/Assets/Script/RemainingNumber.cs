df530e790736ea97a7af9334f5e02b8731ea85d5
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-24 11:35:12 +0900

    演算子の残り使用回数の表示。

---
+++ ProjectHiramath/Assets/Script/RemainingNumber.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class RemainingNumber : MonoBehaviour {
+    GameObject blockarray;
+    public int operatorNum;//演算子番号
+    // Use this for initialization
+	void Start () {
+        blockarray = GameObject.Find("BlockBoxArray");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int hoge = blockarray.GetComponent<BlockBox>().NumberLastCheck(operatorNum);
+        this.GetComponent<Text>().text = "×" + blockarray.GetComponent<BlockBox>().NumberLastCheck(operatorNum);
+	
+	}
+}
