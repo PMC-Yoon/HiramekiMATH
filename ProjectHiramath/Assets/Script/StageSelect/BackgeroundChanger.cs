370e1c05c47d71e1bcb14c9758e3d240947ae0b3
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-25 13:33:30 +0900

    ステージセレクトUIを一新。 Characterによって背景がかわったりするようになった

---
+++ ProjectHiramath/Assets/Script/StageSelect/BackgeroundChanger.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgeroundChanger : MonoBehaviour {
+
+    public Sprite gari;
+    public Sprite nyuton;
+    public Sprite ain;
+    public Sprite aruki;
+
+    // Use this for initialization
+    void Start () {
+
+       switch( CharacterSelectSystem.SelectCharacter)
+        {
+            case 0://Galileo
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = gari;
+                    break;
+                }
+
+            case 1://あるきめです
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = aruki;
+                    break;
+                }
+            case 2:
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = ain;
+                    break;
+                }
+            case 3:
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = nyuton;
+                    break;
+                }
+
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
