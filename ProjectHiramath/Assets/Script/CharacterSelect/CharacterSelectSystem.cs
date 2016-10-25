3e6b56aa1f80c988fa68dbdbb18e247befb097a0
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-25 15:18:59 +0900

    ・キャラクターセレクトUI一新 ・Skill説明、キャラ説明スペースの表示実装。

--- ProjectHiramath/Assets/Script/CharacterSelect/CharacterSelectSystem.cs
+++ ProjectHiramath/Assets/Script/CharacterSelect/CharacterSelectSystem.cs
@@ -6,14 +6,34 @@ using System.Collections;
 public class CharacterSelectSystem : MonoBehaviour {
     public static int SelectCharacter;
 
+    void Start()
+    {
+        SelectCharacter = 0;
+        GameObject.Find("SelectCharacterPanel").GetComponent<SelectCharacterPanelController>().CharacterPanelDataSet();
+    }
+
 
     public void SetCharacter(int CharacterNum)
     {
         if (CharacterNum >= 0 || CharacterNum >= 3)
         {
             SelectCharacter = CharacterNum;
-            GameObject.Find("Fade").gameObject.GetComponent<Fade>().FadeStart();
+            GameObject.Find("SelectCharacterPanel").GetComponent<SelectCharacterPanelController>().CharacterPanelDataSet();
+          //  GameObject.Find("Fade").gameObject.GetComponent<Fade>().FadeStart();
         }
+
+    }
+
+
+    public void CharacterDecide()
+    {
+         GameObject.Find("Fade").gameObject.GetComponent<Fade>().FadeStart();
+    }
+
+    public void TitleBack()
+    {
+        GameObject.Find("Fade").gameObject.GetComponent<Fade>().NextSceneName = "title";
+        GameObject.Find("Fade").gameObject.GetComponent<Fade>().FadeStart();
 
     }
 }
---
+++ ProjectHiramath/Assets/Script/CharacterSelect/SelectCharacterPanelController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectCharacterPanelController : MonoBehaviour {
+
+    //キャラクターの表示画像
+    public Sprite GalileoSprite;
+    public Sprite EinsteinSprite;
+    public Sprite NewtonSprite;
+    public Sprite ArchimedesSprite;
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void CharacterPanelDataSet()
+    {
+        switch (CharacterSelectSystem.SelectCharacter)
+        {
+            case 0: //Galileo
+                {
+                    transform.FindChild("explanation").gameObject.GetComponent<Text>().text = "Galileoの説明文";
+                    transform.FindChild("Skillexplanation").gameObject.GetComponent<Text>().text = "GalileoのSkill説明文";
+                    transform.FindChild("CharacterName").gameObject.GetComponent<Text>().text = "ガリレオ";
+                    transform.FindChild("CharacterImage").gameObject.GetComponent<Image>().sprite = GalileoSprite;
+                    break;
+                }
+            case 1: //Archimedes
+                {
+                    transform.FindChild("explanation").gameObject.GetComponent<Text>().text = "Archimedesの説明文";
+                    transform.FindChild("Skillexplanation").gameObject.GetComponent<Text>().text = "ArchimedesのSkill説明文";
+                    transform.FindChild("CharacterName").gameObject.GetComponent<Text>().text = "アルキメデス";
+                    transform.FindChild("CharacterImage").gameObject.GetComponent<Image>().sprite = ArchimedesSprite;
+                    break;
+                }
+            case 2: //Einstein
+                {
+                    transform.FindChild("explanation").gameObject.GetComponent<Text>().text = "Einsteinの説明文";
+                    transform.FindChild("Skillexplanation").gameObject.GetComponent<Text>().text = "EinsteinのSkill説明文";
+                    transform.FindChild("CharacterName").gameObject.GetComponent<Text>().text = "アインシュタイン";
+                    transform.FindChild("CharacterImage").gameObject.GetComponent<Image>().sprite = EinsteinSprite;
+                    break;
+                }
+            case 3: //Newton
+                {
+                    transform.FindChild("explanation").gameObject.GetComponent<Text>().text = "Newtonの説明文";
+                    transform.FindChild("Skillexplanation").gameObject.GetComponent<Text>().text = "NewtonのSkill説明文";
+                    transform.FindChild("CharacterName").gameObject.GetComponent<Text>().text = "ニュートン";
+                    transform.FindChild("CharacterImage").gameObject.GetComponent<Image>().sprite = NewtonSprite;
+                    break;
+                }
+
+        }
+
+    }
+	
+
+}
