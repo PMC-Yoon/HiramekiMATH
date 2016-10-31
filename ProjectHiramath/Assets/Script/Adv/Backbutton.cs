2337930cc668fc353e7a4af5ba3894c3e63483fe
kazuaki,terabayashi <terabayashi@pmc.co.jp>
2016-10-31 11:20:58 +0900

    ADVパートに！待望の！！！スキップ！！！ボタン！！！！！！！！！！！！！！！！！！！！

---
+++ ProjectHiramath/Assets/Script/Adv/Backbutton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backbutton : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void BackButton()
+    {
+        GameObject.Find("Fade").GetComponent<Fade>().NextSceneName = "StageSelect";
+        GameObject.Find("Fade").GetComponent<Fade>().FadeStart();
+
+    }
+
+}
